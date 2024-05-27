95ae9cf081a83ceb3b889d89f46a818b8373b88e
Arthur Felipe Pallu <arthurfelipepallu@gmail.com>
2024-05-27 14:00:08 -0300

    Header feito e componentes iniciados

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+export default function Header()
+{
+    return (
+        <>
+            <div className="dpr-container dpr-header">
+                <p className="dpr-title">DSCarrosTop</p>
+            </div>
+        </>
+    );
+}
---
+++ src/routes/FirstPage/index.tsx
@@ -0,0 +1,16 @@
+import Header from "../../Components/Header";
+import "./styles.css";
+export default function FirstPage() {
+  return (
+    <>
+      <Header />
+      <body>
+        <main>
+          <section id="FirstPage-section" className="container">
+            <p className="chamado"> Venha nos visitar</p>
+          </section>
+        </main>
+      </body>
+    </>
+  );
+}
