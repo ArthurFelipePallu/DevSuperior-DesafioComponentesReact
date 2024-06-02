71c056a05aaefee5becc53fc0871fe75048cbd01
Arthur Felipe Pallu <arthurfelipepallu@gmail.com>
2024-06-02 11:52:43 -0300

    FInalizado

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <>
       <div className="dpr-footer-container dpr-mt25">
-        <div className=" dpr-contact-box">
+        <div className=" dpr-contact-box dpr-padtp35">
           <p className="dpr-contact-fonts ">@dscarrostop</p>
           <p className="dpr-contact-fonts dpr-font-italic">Rua da Pipoca, 150</p>
         </div>
